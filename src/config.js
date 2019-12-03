c8a92b6f44675da7e51caa86f9e4f24e53c60883
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-12-03 20:50:43 +0000

    changed api endpoint to heroku

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://intense-cliffs-98344.herokuapp.com/',
   TOKEN_KEY: 'booktracker-client-auth-token',
 }
