4ba6bf93a88c99b4f02f0aa8f3b1dc650216c092
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2021-04-01 01:18:35 -0400

    eslint fix

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   //API_ENDPOINT: 'http://localhost:8000/api',
   API_ENDPOINT: 'https://gentle-fortress-47962.herokuapp.com/api',
